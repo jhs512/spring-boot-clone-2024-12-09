8965c504cce301005d78441d56f594a6aae5a3c1
jhs512 <jangka512@gmail.com>
2024-12-09 00:33:34 +0900

    ApplicationContext, g

---
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -0,0 +1,7 @@
+package com.ll.framework.ioc;
+
+public class ApplicationContext {
+    public ApplicationContext(String basePackage) {
+        System.out.println("ApplicationContext: " + basePackage);
+    }
+}
