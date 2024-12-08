83f3df6fe65a45747df3374db0ecef435e4b5366
jhs512 <jangka512@gmail.com>
2024-12-09 00:41:56 +0900

    t2, r

--- src/test/java/com/ll/framework/ioc/ApplicationContextTest.java
+++ src/test/java/com/ll/framework/ioc/ApplicationContextTest.java
@@ -1,13 +1,30 @@
 package com.ll.framework.ioc;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class ApplicationContextTest {
+    private static ApplicationContext applicationContext;
+
+    @BeforeAll
+    public static void beforeAll() {
+        applicationContext = new ApplicationContext("com.ll");
+    }
+
     @Test
     @DisplayName("ApplicationContext")
     public void t1() {
-        ApplicationContext applicationContext = new ApplicationContext("com.ll");
         System.out.println(applicationContext);
+    }
+
+    @Test
+    @DisplayName("applicationContext.findComponentBy(TestPostService.class);")
+    public void t2() {
+        Class<TestPostService> cls = applicationContext.findComponentBy(TestPostService.class);
+
+        assertThat(cls).isNotNull();
     }
 }
